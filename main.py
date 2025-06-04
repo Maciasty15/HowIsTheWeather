b54ffaf18aadd1a4fe7c4eb5c6f666d2b3904ef7
whajdusianek <wojtek243@gmail.com>
2025-06-04 20:22:43 +0200

    doqMegVY3

--- main.py
+++ main.py
@@ -1,16 +1,3 @@
-
-#rozwiązanie problemu niewidzących się folderów na tym samym poziomie 
-# import os
-# import sys
-# PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-# SRC_DIR = os.path.join(PROJECT_ROOT, 'src')
-# SCRIPTS_DIR = os.path.join(PROJECT_ROOT, 'scripts')
-# TESTS_DIR = os.path.join(PROJECT_ROOT, 'tests')
-# sys.path.insert(0, SRC_DIR)
-# sys.path.insert(0, SCRIPTS_DIR)
-# sys.path.insert(0, TESTS_DIR)
-
-
 from src.utils.logger import get_logger
 
 logger = get_logger(__name__)
